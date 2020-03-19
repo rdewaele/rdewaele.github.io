9e1efbf16adf5870885edd3e1d65611aa3ee6d2d
Robrecht Dewaele <1044505+rdewaele@users.noreply.github.com>
2020-03-20 00:27:55 +0100

    .env should not be ignored

--- bootstrap-repository.sh
+++ bootstrap-repository.sh
@@ -22,7 +22,6 @@ patch -Np0 "$directory"/Gemfile < Gemfile.patch
 docker run -v $PWD/"$directory":/srv/jekyll jekyll/jekyll:3.8.5 bundle update
 
 echo "DIRECTORY=$directory" > .env
-echo ".env" >> .gitignore
 
 # patch rejected file
 echo "*Gemfile.rej" >> .gitignore
